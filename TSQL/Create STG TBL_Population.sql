8c548be5a8e3de11ae5286910668f92c8eb2ff93
rickscole <ricksaubercole@gmail.com>
2022-04-06 17:30:08 -0400

    Create Create STG TBL_Population.sql

---
+++ TSQL/Create STG TBL_Population.sql
@@ -0,0 +1,15 @@
+/****** Object:  Table [STG].[TBL_Population]    Script Date: 4/6/2022 3:28:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [STG].[TBL_Population](
+	[Population] [nvarchar](100) NULL,
+	[Economy] [nvarchar](100) NULL,
+	[Time] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
+
+
