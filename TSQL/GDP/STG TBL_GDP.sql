4bb7dcc49a872f047569efc355e65bc93cd59551
rickscole <ricksaubercole@gmail.com>
2022-04-07 11:12:05 -0400

    Create STG TBL_GDP.sql

---
+++ TSQL/GDP/STG TBL_GDP.sql
@@ -0,0 +1,14 @@
+/****** Object:  Table [STG].[TBL_GDP]    Script Date: 4/7/2022 9:11:18 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [STG].[TBL_GDP](
+	[GDP] [nvarchar](100) NULL,
+	[Economy] [nvarchar](100) NULL,
+	[Time] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
+
