2fd5f78e90a356746b4993bc03e0b017e62bb98c
rickscole <ricksaubercole@gmail.com>
2022-04-07 11:01:27 -0400

    Create TBL_CountryNameTranslations.sql

---
+++ TSQL/General/TBL_CountryNameTranslations.sql
@@ -0,0 +1,16 @@
+/****** Object:  Table [WB].[TBL_CountryNameTranslations]    Script Date: 4/7/2022 9:00:58 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [WB].[TBL_CountryNameTranslations](
+	[Code] [varchar](50) NULL,
+	[Name] [varchar](500) NULL,
+	[GeographyCode] [varchar](50) NULL,
+	[IncomeCode] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
